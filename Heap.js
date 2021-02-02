9452b0efb27b05cc666f5b55cb7c213b9573f0dd
aikusoni <31752803+aikusoni@users.noreply.github.com>
2021-02-02 16:54:52 +0900

    Add files via upload

---
+++ Heap.js
@@ -0,0 +1,54 @@
+class Heap {    
+    constructor(comparator) {
+        this.heap = [];
+        this.comparator = comparator;
+    }
+    
+    size() {
+        return this.heap.length;
+    }
+    
+    peek() {
+        return this.heap.length === 0 ? null : this.heap[0];
+    }
+    
+    poll() {
+        let topE = this.heap.shift();
+        let popE = this.heap.pop();
+        if (popE) {
+            this.heap.unshift(popE);
+            let c = 0;
+            while (true) {
+                let l = c * 2 + 1;
+                let r = c * 2 + 2;
+                if (this.heap[l] && (this.comparator(this.heap[c], this.heap[l]) > 0)
+                    || this.heap[r] && (this.comparator(this.heap[c], this.heap[r]) > 0)) {
+                    let t = (!this.heap[r] || this.comparator(this.heap[r], this.heap[l]) > 0)? l : r;
+                    [this.heap[t], this.heap[c]] = [this.heap[c], this.heap[t]];
+                    c = t;
+                } else {
+                    break;
+                }
+            }
+        }
+        return topE ? topE : null;
+    }
+    
+    add(newE) {
+        this.heap.push(newE);
+        let c = this.heap.length - 1;
+        while (true) {
+            let p = Math.floor((c-1)/2);
+            if (c > 0 && (this.comparator(this.heap[c], this.heap[p]) < 0)) {
+                [this.heap[c], this.heap[p]] = [this.heap[p], this.heap[c]];
+                c = p;
+            } else {
+                break;
+            }
+        }
+    }
+    
+    toString() {
+        return this.heap + "";
+    }
+}
